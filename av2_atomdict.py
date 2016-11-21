00161310eccaa8a5952d409737693fa8175f11ea
MKorablyov <MKorablyov@gmail.com>
2016-11-21 01:33:36 -0500

    initial upload of affinity V3 core code

---
+++ av2_atomdict.py
@@ -0,0 +1,49 @@
+class atom_dictionary:
+    LIG = {}
+
+    LIG["h"] = 1; LIG["h1"] = 1; LIG["h2"] = 1; LIG["h3"] = 1; LIG["h4"] = 1; LIG["h5"] = 1; LIG["h6"] = 1; LIG["h7"] = 1;
+    LIG["h8"] = 1; LIG["h1*"] = 1; LIG["h2*"] = 1; LIG["h3*"] = 1; LIG["h4*"] = 1; LIG["h5*"] = 1; LIG["h6*"] = 1;
+    LIG["h7*"] = 1; LIG["h8*"] = 1; LIG["hg"] = 1; LIG["hxt"] = 1; LIG["hz1"] = 1; LIG["hz2"] = 1; LIG["he2"] = 1;
+    LIG["d"] = 1
+
+    LIG["b"] = 5
+
+    LIG["c"] = 6; LIG["c1"] = 6; LIG["c2"] = 6; LIG["c3"] = 6; LIG["c4"] = 6; LIG["c5"] = 6; LIG["c6"] = 6; LIG["c7"] = 6;
+    LIG["c8"] = 6; LIG["c1*"] = 6; LIG["c2*"] = 6; LIG["c3*"] = 6; LIG["c4*"] = 6; LIG["c5*"] = 6; LIG["c7*"] = 6;
+    LIG["c8*"] = 6; LIG["cb"] = 6; LIG["ca"] = 6; LIG["ce"] = 6; LIG["cg"] = 6; LIG["cd"] = 6; LIG["cd1"] = 6;
+    LIG["cd2"] = 6;
+
+    LIG["n"] = 7; LIG["n1"] = 7; LIG["n2"] = 7; LIG["n3"] = 7; LIG["n4"] = 7; LIG["n5"] = 7; LIG["n6"] = 7;
+    LIG["n7"] = 7; LIG["n8"] = 7; LIG["nz"] = 7;
+
+    LIG["o"] = 8; LIG["o1"] = 8; LIG["o2"] = 8; LIG["o3"] = 8; LIG["o4"] = 8; LIG["o5"] = 8; LIG["o6"] = 8;
+    LIG["o7"] = 8; LIG["o8"] = 8; LIG["o1*"] = 8; LIG["o2*"] = 8; LIG["o3*"] = 8; LIG["o4*"] = 8; LIG["o5*"] = 8;
+    LIG["o6*"] = 8; LIG["o7*"] = 8; LIG["o8*"] = 8; LIG["oe1"] = 8; LIG["oe2"] = 8; LIG["cd1"] = 8; LIG["oxt"] = 8;
+
+    LIG["f"] = 9; LIG["p"] = 7.2; LIG["s"] = 8.2; LIG["sg"] = 8.2; LIG["cl"] = 9.2; LIG["v"] = 12; LIG["se"] = 8.4;
+    LIG["br"] = 9.4; LIG["i"] = 9.6
+
+
+    REC = {}
+
+    REC["h"] = 21 ; REC["h1"] = 21; REC["h2"] = 21; REC["h3"] = 21; REC["h4"] = 21; REC["h5"] = 21; REC["h6"] = 21;
+    REC["h7"] = 21; REC["h8"] = 21; REC["h1*"] = 21; REC["h2*"] = 21; REC["h3*"] = 21; REC["h4*"] = 21; REC["h5*"] = 21;
+    REC["h6*"] = 21; REC["h7*"] = 21; REC["h8*"] = 21; REC["hg"] = 21; REC["hxt"] = 21; REC["hz1"] = 21; REC["hz2"] = 21;
+    REC["he2"] = 21; REC["d"] = 21
+
+    REC["b"] = 5
+
+    REC["c"] = 26; REC["c1"] = 26; REC["c2"] = 26; REC["c3"] = 26; REC["c4"] = 26; REC["c5"] = 26; REC["c6"] = 26;
+    REC["c7"] = 26; REC["c8"] = 26; REC["c1*"] = 26; REC["c2*"] = 26; REC["c3*"] = 26; REC["c4*"] = 26; REC["c5*"] = 26;
+    REC["c7*"] = 26; REC["c8*"] = 26; REC["cb"] = 26; REC["ca"] = 26; REC["ce"] = 26; REC["cg"] = 26; REC["cd"] = 26;
+    REC["cd1"] = 26; REC["cd2"] = 26;
+
+    REC["n"] = 27; REC["n1"] = 27; REC["n2"] = 27; REC["n3"] = 27; REC["n4"] = 27; REC["n5"] = 27; REC["n6"] = 27;
+    REC["n7"] = 27; REC["n8"] = 27; REC["nz"] = 27;
+
+    REC["o"] = 28; REC["o1"] = 28; REC["o2"] = 28; REC["o3"] = 28; REC["o4"] = 28; REC["o5"] = 28; REC["o6"] = 28;
+    REC["o7"] = 28; REC["o8"] = 28; REC["o1*"] = 28; REC["o2*"] = 28; REC["o3*"] = 28; REC["o4*"] = 28; REC["o5*"] = 28;
+    REC["o6*"] = 28; REC["o7*"] = 28; REC["o8*"] = 28; REC["oe1"] = 28; REC["oe2"] = 28; REC["cd1"] = 28; REC["oxt"] = 28;
+
+    REC["f"] = 29; REC["p"] = 27.2; REC["s"] = 28.2; REC["sg"] = 28.2; REC["cl"] = 29.2; REC["v"] = 32; REC["se"] = 28.4;
+    REC["br"] = 29.4; REC["i"] = 29.6
