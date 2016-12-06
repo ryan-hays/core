c8274ab6f3254f0e1d9b94be5331c30477bbfc41
MKorablyov <MKorablyov@gmail.com>
2016-12-06 16:53:37 -0500

    few comments

--- av3_eval.py
+++ av3_eval.py
@@ -190,13 +190,18 @@ class store_predictions:
         f.write(str(self.confusion_matrix(prediction_averages,self.labels)))
         f.close()
 
-        # write submission
-
+        # write a file in Kaggle MAP{K} submision format
+        # the form is:
+        # Protein1, Ligand3 Ligand4 Ligand2
+        # Protein2, Ligand5 Ligand9 Ligand7
+        
         submission = {}
         for i in range(num_examples):
+            # get the name of the ligand and protein
             ligand,receptor = self.pl_pairs[i].split(',')
             ligand = ligand.split('/')[-1].split('.')[0]
             receptor = receptor.split('/')[-1].split('.')[0]
+            # add all protein-ligand pairs to submission
             if not receptor in submission.keys():
                 submission[receptor] = {}
                 submission[receptor]['ligands'] = [ligand]
@@ -204,7 +209,8 @@ class store_predictions:
             else:
                 submission[receptor]['ligands'].append(ligand)
                 submission[receptor]['score'].append(prediction_averages[i])
-
+        
+        # write and save submisison to file
         with open(file_path+'_submission.txt','w') as f:
             f.write('Id,Prediction\n')
             for key in submission.keys():
