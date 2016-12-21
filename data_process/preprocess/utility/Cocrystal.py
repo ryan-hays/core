2317354dd9f64b782c04fa8df06a345d55852d1e
wy <panzer.wy@gmail.com>
2016-12-21 00:48:50 -0500

    Add method blueprints for this file.

--- data_process/preprocess/utility/Cocrystal.py
+++ data_process/preprocess/utility/Cocrystal.py
@@ -15,3 +15,60 @@ class PDB_container:
     I hope this class will only have methods as APIs for other programs or users,
     so it will not deal with files and hopefully return some nice results.
     '''
+
+    def __init__(self,filename,fileloc=None,pdbname=None):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def set_config(self,config):
+        '''
+
+        :param config: a dict for configuration that will be applied
+        :return:
+        '''
+
+    instance_index = 0
+    @classmethod
+    def get_num_of_instance(cls):
+        cls.instance_index += 1
+        return cls.instance_index
+
+    def get_pdb_info_dict(self):
+        pass
+
+    def get_pdb_info_str(self):
+        pass
+
+    def get_ligand_ids(self):
+        pass
+
+    def get_ligands_info_dict(self):
+        pass
+
+    def get_ligand_info_dict(self,ligand_id):
+        pass
+
+    def get_ligand_info_str(self,ligand_id):
+        pass
+
+    def get_ligands_similarity(self,ligand_id, src_file, src_filedir=None):
+        pass
+
+    def get_ligand_similarity(self,ligand_id, src_file, src_filedir=None):
+        pass
+
+    def get_ligand_autovina_score(self,ligand_id):
+        pass
+
+    def get_ligand_autogrid_maps(self,ligand_id):
+        pass
+
+
+class Docking_Analyzer(PDB_container):
+    '''
+    Inherit from PDB_container, but specialized with docking remark generation
+    '''
+    pass
+
