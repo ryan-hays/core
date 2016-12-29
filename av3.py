1ce1654d906785a7f42941bc4ebdc11494a74fe9
Ubuntu <ubuntu@ip-172-31-4-5.us-west-2.compute.internal>
2016-12-29 22:42:03 +0000

    update test cost functions

--- av3.py
+++ av3.py
@@ -1,4 +1,4 @@
-import time,os
+import time,os, math
 import tensorflow as tf
 import numpy as np
 from av3_input import launch_enqueue_workers
@@ -81,35 +81,31 @@ def max_net(x_image_batch,keep_prob):
     with tf.name_scope('input'):
         pass
     with tf.name_scope("input_reshape"):
-        print "image batch dimensions", x_image_batch.get_shape()
-        # formally adding one depth dimension to the input
-        x_image_with_depth = tf.reshape(x_image_batch, [-1, 20, 20, 20, 1])
-        print "input to the first layer dimensions", x_image_with_depth.get_shape()
+        # reshaping 8000 vector into 20*20*20 image
+        x_image = tf.reshape(x_image_batch, [-1, 20, 20, 20, 1])
 
-    h_conv1 = conv_layer(layer_name='conv1_2x2x2', input_tensor=x_image_with_depth, filter_size=[2, 2, 2, 1, 30])
-    h_relu1 = relu_layer(layer_name='relu1', input_tensor=h_conv1)
-    h_pool1 = pool_layer(layer_name='pool1_2x2x2', input_tensor=h_relu1, ksize=[1, 2, 2, 2, 1], strides=[1, 2, 2, 2, 1])
 
-    h_conv2 = conv_layer(layer_name="conv2_2x2x2", input_tensor=h_pool1, filter_size=[2, 2, 2, 30, 45])
-    h_relu2 = relu_layer(layer_name="relu2", input_tensor=h_conv2)
-    h_pool2 = pool_layer(layer_name="pool2_2x2x2", input_tensor=h_relu2, ksize=[1, 2, 2, 2, 1], strides=[1, 1, 1, 1, 1])
-
-    h_conv3 = conv_layer(layer_name="conv3_2x2x2", input_tensor=h_pool2, filter_size=[2, 2, 2, 45, 60])
-    h_relu3 = relu_layer(layer_name="relu3", input_tensor=h_conv3)
-    h_pool3 = pool_layer(layer_name="pool3_2x2x2", input_tensor=h_relu3, ksize=[1, 2, 2, 2, 1], strides=[1, 1, 1, 1, 1])
-
-    h_conv4 = conv_layer(layer_name="conv4_2x2x2", input_tensor=h_pool3, filter_size=[2, 2, 2, 60, 75])
-    h_relu4 = relu_layer(layer_name="relu4", input_tensor=h_conv4)
-    h_pool4 = pool_layer(layer_name="pool4_2x2x2", input_tensor=h_relu4, ksize=[1, 2, 2, 2, 1], strides=[1, 1, 1, 1, 1])
-
-    h_conv5 = conv_layer(layer_name="conv5_2x2x2", input_tensor=h_pool4, filter_size=[2, 2, 2, 75, 90])
-    h_relu5 = relu_layer(layer_name="relu5", input_tensor=h_conv5)
-    h_pool5 = pool_layer(layer_name="pool5_2x2x2", input_tensor=h_relu5, ksize=[1, 2, 2, 2, 1], strides=[1, 1, 1, 1, 1])
+    #making different regions
+    inp_tensor = x_image
+    max_no_layers = 20
+    no_layers = 0
+    inp_size = 1
+    out_size = 30
+    current_matrix_edge_size = 20
+    while no_layers < max_no_layers:
+        h_conv = conv_layer(layer_name='conv'+str(no_layers)+'_2x2x2', input_tensor=inp_tensor, filter_size=[2, 2, 2, inp_size, out_size])
+        inp_tensor = relu_layer(layer_name='relu'+str(no_layers), input_tensor=h_conv)
+        if no_layers < 3:
+                inp_tensor = pool_layer(layer_name='pool'+str(no_layers)+'_2x2x2', input_tensor=inp_tensor, ksize=[1, 2, 2, 2, 1], strides=[1, 2, 2, 2, 1])
+                current_matrix_edge_size = max(int(current_matrix_edge_size+1)/2, 1)
+        inp_size = out_size
+        out_size = out_size + 15
+        no_layers += 1
 
     with tf.name_scope("flatten_layer"):
-        h_pool2_flat = tf.reshape(h_pool5, [-1, 10 * 10 * 10 * 90])
+        h_pool4_flat = tf.reshape(inp_tensor, [-1, current_matrix_edge_size*current_matrix_edge_size*current_matrix_edge_size * inp_size])
 
-    h_fc1 = fc_layer(layer_name="fc1", input_tensor=h_pool2_flat, output_dim=1024)
+    h_fc1 = fc_layer(layer_name="fc1", input_tensor=h_pool4_flat, output_dim=1024)
     h_fc1_relu = relu_layer(layer_name="fc1_relu", input_tensor=h_fc1)
 
     with tf.name_scope("dropout"):
@@ -120,29 +116,25 @@ def max_net(x_image_batch,keep_prob):
     h_fc2_relu = relu_layer(layer_name="fc2_relu", input_tensor=h_fc2)
 
     y_conv = fc_layer(layer_name="out_neuron", input_tensor=h_fc2_relu, output_dim=2)
-
     return y_conv
 
 
-#def compute_weighted_cross_entropy_mean(logits, labels,batch_size):
-#    """computes weighted cross entropy mean for a two class classification.
-#    Applies tf.nn.weighted_cross_entropy_with_logits
-#    accepts "labels" instead of "targets" as in
-#    tf.nn.sparse_softmax_cross_entropy_with_logits"""
-#
-#    with tf.name_scope('weighted_cross_entropy_mean'):  # TODO (multiclass) now hardcoded to two classes
-#
-#        # first column is inverted labels, second column is labels
-#        # batch_class_zero = tf.reshape(-labels + 1, [batch_size, 1])
-#        batch_class_one = tf.reshape(labels, [batch_size, 1])
-#        print "shape of logits", logits.get_shape()
-#        # logits_class_one = tf.reshape(logits, [batch_size, 1])
-#        # batch_y = tf.concat(1, (batch_class_zero, batch_class_one))
-#
-#        weighted_cross_entropy = tf.nn.weighted_cross_entropy_with_logits(logits, batch_class_one, pos_weight=50, name=None)
-#
-#        cross_entropy_mean = tf.reduce_mean(weighted_cross_entropy, name='cross_entropy')
-#        return cross_entropy_mean
+def compute_weighted_cross_entropy_mean(logits, labels,batch_size):
+    """computes weighted cross entropy mean for a two class classification.
+    Applies tf.nn.weighted_cross_entropy_with_logits
+    accepts "labels" instead of "targets" as in
+    tf.nn.sparse_softmax_cross_entropy_with_logits"""
+
+    with tf.name_scope('weighted_cross_entropy_mean'):  # TODO (multiclass) now hardcoded to two classes
+
+        # first column is inverted labels, second column is labels
+        batch_class_zero = tf.reshape(-labels + 1, [batch_size, 1])
+        batch_class_one = tf.reshape(labels, [batch_size, 1])
+        batch_y = tf.concat(1, (batch_class_zero, batch_class_one))
+        weighted_cross_entropy = tf.nn.weighted_cross_entropy_with_logits(logits, batch_y, pos_weight=5000, name=None)
+
+        cross_entropy_mean = tf.reduce_mean(weighted_cross_entropy, name='cross_entropy')
+        return cross_entropy_mean
 
 
 #def unbalanced_sparse_softmax_cross_entropy_with_logits(logits,targets,class_weights=[1,1]):
@@ -171,16 +163,14 @@ def max_net(x_image_batch,keep_prob):
 
 
 def unbalanced_sparse_softmax_cross_entropy_with_logits(logits,labels,class_weights=[1,1]):
-
-
     # convert labels to targets first
     batch_size = int(logits.get_shape()[0])
     num_classes = int(logits.get_shape()[1])
     # because the default format of labels is float32, it needs to be converted to int64
     labels = tf.cast(labels,dtype=tf.int32)
 
+    print("calc softmax")
     indices = tf.cast(tf.pack((tf.range(0,batch_size),labels),axis=1),dtype=tf.int64)
-
 
     sparse_targets = tf.SparseTensor(indices=indices, values=tf.ones(batch_size,dtype=tf.float32),shape=[batch_size,num_classes])
     targets = tf.sparse_tensor_to_dense(sparse_targets)
@@ -200,32 +190,49 @@ def unbalanced_sparse_softmax_cross_entropy_with_logits(logits,labels,class_weig
 
     max_logits = tf.reduce_max(logits, reduction_indices=1)
     soft_maximum_x = max_logits + tf.log(tf.reduce_sum(tf.exp(logits - tf.tile(tf.reshape(max_logits,shape=[batch_size,1]),multiples=[1,num_classes])),1))
-
     simple_entropy = targets * -(logits - tf.tile(tf.reshape(soft_maximum_x,shape=[batch_size,1]),multiples=[1,num_classes]))
     unbalanced_entropy = tf.reduce_sum(class_weights * simple_entropy,reduction_indices=1)
-
     return unbalanced_entropy
 
+#def imbalanced_dequeue(queue, keepprob):
+#    count=0
+#    y_ = tf.zeros(FLAGS.batch_size)
+#    x_image_batch = tf.tile(tf.zeros(8000), FLAGS.batch_size)
+#    while count < FLAGS.batch_size:
+	#assuming queue has been randomized
+#	y, x, _,_ = train_image_queue.dequeue()
+#	add = True
+#	if tf.equal(y, tf.constant(0)) and math.rand() < keepprob:
+#	   add = False
 
-
-
+#	if add:
+#	    y_sparse = tf.SparseTensor(count, y, tf.shape(y_))
+#	    y_ = tf.add(y_, tf.sparse_tensor_to_dense(y_sparse))
+#	    indices = tf.add(tf.tile(count, 8000), tf.range(0, 8000))
+#	    x_sparse = tf.SparseTensor(indices,x,tf.shape(x_image_batch))
+#	    x_image_batch = tf.add(x_image_batch, tf.sparse_tensor_to_dense(x_sparse))
+#	    count+=1
+#    return (y_,x_image_batch)
 
 def train():
     "train a network"
-
+    print("opening")
     # create session since everything is happening in one
     sess = tf.Session()
+    print("b4 LE")
     train_image_queue,filename_coordinator = launch_enqueue_workers(sess=sess, pixel_size=FLAGS.pixel_size, side_pixels=FLAGS.side_pixels, num_workers=FLAGS.num_workers, batch_size=FLAGS.batch_size,
                                          database_index_file_path=FLAGS.train_set_file_path, num_epochs=FLAGS.num_epochs)
-
+    print("after launch enqueue")
     y_, x_image_batch,_,_ = train_image_queue.dequeue_many(FLAGS.batch_size)
     keep_prob = tf.placeholder(tf.float32)
     y_conv = max_net(x_image_batch, keep_prob)
 
     # todo
+    #cross_entropy_mean = compute_weighted_cross_entropy_mean(y_conv, y_, FLAGS.batch_size) 
     cross_entropy = unbalanced_sparse_softmax_cross_entropy_with_logits(y_conv,y_,[1,50])
+    
     cross_entropy_mean = tf.reduce_sum(cross_entropy) / FLAGS.batch_size
-
+    
 
     with tf.name_scope('train'):
         tf.scalar_summary('unbalanced cross entropy mean', cross_entropy_mean)
@@ -250,18 +257,18 @@ def train():
     train_writer = tf.train.SummaryWriter((FLAGS.summaries_dir + '/' + str(FLAGS.run_index) + "_train"), sess.graph)
     # test_writer = tf.train.SummaryWriter(FLAGS.summaries_dir + '/test' + str(FLAGS.run_index))
 
-
+    print("var init")
     # initialize all variables
     sess.run(tf.initialize_all_variables())
 
     batch_num = 0
+    print("here")
     while not filename_coordinator.stop:
-        start = time.time()
+        print("init")
+	start = time.time()
         training_error,_ = sess.run([cross_entropy_mean,train_step_run],feed_dict={keep_prob:0.5})
-
         print "step:", batch_num, "run error:", training_error,\
             "examples per second:", "%.2f" % (FLAGS.batch_size / (time.time() - start))
-
         # once in a hundred batches calculate correct predictions
         if (batch_num % 1000 == 999):
             # evaluate and print a few things
@@ -275,7 +282,6 @@ def train():
         # exit the loop in case there is something wrong with the setup and model diverged into inf
         assert not np.isnan(training_error), 'Model diverged with loss = NaN'
         batch_num+=1
-
 
 
 
@@ -297,9 +303,9 @@ class FLAGS:
 
     # data directories
     # path to the csv file with names of images selected for training
-    train_set_file_path = '../datasets/filter_rmsd_atoms/train_set.csv'
+    train_set_file_path = '../../common/datasets/labeled_pdb/train_set_short.csv'
     # path to the csv file with names of the images selected for testing
-    test_set_file_path = '../datasets/filter_rmsd_atoms/test_set.csv'
+    test_set_file_path = '../../common/datasets/labeled_pdb/test_set_short.csv'
     # directory where to write variable summaries
     summaries_dir = './summaries'
 
