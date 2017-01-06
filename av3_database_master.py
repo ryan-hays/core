d8aab28be093ea830fc31fccec63ed5692d0a2be
Maksym Korablyov <mkorablyov@gmail.com>
2017-01-06 13:02:35 -0500

    oversampler

--- av3_database_master.py
+++ av3_database_master.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os,re, random,prody
+import os,re, random,prody,time
 from itertools import chain
 from av2_atomdict import atom_dictionary
 
@@ -14,9 +14,15 @@ from av2_atomdict import atom_dictionary
 # todo some error handling and loggig
 # todo add time.time
 
+
+
+
 def preprocess_PDB_to_npy(database_path):
     """crawls the folder (receptors in this case) and saves every PDB it finds
     into .npy array with 1) coordinates 2) mapped to the atom name number """
+    class statistics:
+        docked_ligands_overlap = 0
+        docked_ligands_not_overlap = 0
 
     def preprocess_ligands(folder_path):
         for dirpath, dirnames, filenames in os.walk(folder_path):
@@ -35,9 +41,21 @@ def preprocess_PDB_to_npy(database_path):
 
                         atom_numbers = map(ligand_atom_to_number, prody_ligand.getElements())
                         coordinates_and_atoms = np.hstack((prody_ligand.getCoords(), np.reshape(atom_numbers, (-1, 1))))
-                        np.save(re.sub('.pdb$', '', ligand_file_path), coordinates_and_atoms)
                     except Exception:
                         pass
+
+
+                    # FIXME fixing clash @Xiao should update the database
+                    if re.search('/docked_ligands/',ligand_file_path):
+                        if not docked_ligand_overlaps_with_crystal(database_path,ligand_file_path):
+                            statistics.docked_ligands_not_overlap +=1
+                            np.save(re.sub('.pdb$', '', ligand_file_path), coordinates_and_atoms)
+                        else:
+                            statistics.docked_ligands_overlap +=1
+                            print "overlap statistics:", statistics.docked_ligands_overlap,"/",statistics.docked_ligands_not_overlap
+                    else:
+                        np.save(re.sub('.pdb$', '', ligand_file_path), coordinates_and_atoms)
+
 
 
     def preprocess_receptors(folder_path):
@@ -57,8 +75,8 @@ def preprocess_PDB_to_npy(database_path):
                     except Exception:
                         pass
 
-    preprocess_ligands(database_path + "/crystal_ligands/")
     preprocess_ligands(database_path + "/docked_ligands/")
+    preprocess_ligands(database_path + "/crystal_ligands/")
     preprocess_receptors(database_path + "/receptors/")
 
 
@@ -67,8 +85,7 @@ def preprocess_PDB_to_npy(database_path):
 def assign_label_from_path(ligand_file_path):                                                                         # todo
     """assigns label to be used for training."""
     # 1 for the crystal structure
-    # 0 for bad fast docking
-    # NONE for good fast docking
+    # 0 for fast docked structure
     label = None
     if re.search('/crystal_ligands/', ligand_file_path):
         if label == None:
@@ -93,12 +110,6 @@ def assign_label_from_path(ligand_file_path):                                   
     else:
         return label
 
-
-    # TODO if both have been assigned
-    # check if one of the labels has been assigned
-
-    #random_label = int(round(random.random()))
-    #return random_label
 
 
 def write_database_index_file(database_path,database_index_path,lig_dirs,label_gen_func=assign_label_from_path,rec_dir="receptors"):
@@ -131,15 +142,12 @@ def write_database_index_file(database_path,database_index_path,lig_dirs,label_g
                             print "WARNING: missing receptor"
 
 
-
-
-
     database_index_file.close()
 
 
-def split_into_train_and_test_sets(database_index_path,train_set_div):
-    """splits the database textfile into training and testing sets. Takes into account that some of the PDBs
-    are multimers. All multimers go i"""
+def split_into_train_and_test_sets(database_index_path,train_set_div,replicate_positives=1):
+    """splits the database textfile into training and testing sets.
+    can replicate positive examples to do oversampling"""
     # create a unique pdb list, and randomly assign every pdb to the training or to the testing set
     database_index_lines = open(database_index_path + "/database_index.csv").readlines()
 
@@ -153,42 +161,89 @@ def split_into_train_and_test_sets(database_index_path,train_set_div):
     test_set = []
     train_set = []
 
+
     def randomly_assign_and_retrieve_example(unique_pdb_name,pdb_names=pdb_names,train_set_div=train_set_div,database_index_lines=database_index_lines):
         # Given a pdb name randomly assign it either to the training or to the testing set keeping the probability.
         # Retrieve all of the examples with this pdb name from the database and write into training/testing file
-
-        for index in np.where(pdb_names == unique_pdb_name)[0]:
-            if (random.random() < train_set_div):
+        # TODO swap "random" to test only
+        if (random.random() > train_set_div):
+            for index in np.where(pdb_names == unique_pdb_name)[0]:
                 test_set.append(database_index_lines[index])
-            else:
-                train_set.append(database_index_lines[index])
+        else:
+            for index in np.where(pdb_names == unique_pdb_name)[0]:
+                label = float(database_index_lines[index].split(",")[0])
+                if (label == 1.0):
+                    [train_set.append(database_index_lines[index]) for i in range(replicate_positives)]
+                else:
+                    train_set.append(database_index_lines[index])
 
     map(randomly_assign_and_retrieve_example,unique_pdb_names)
 
 
-    if not (train_set_div==1):
-        random.shuffle(train_set)
-        open(database_index_path + "/test_set.csv", "w").writelines(train_set)
     if not (train_set_div==0):
         random.shuffle(test_set)
-        open(database_index_path + "/train_set.csv", "w").writelines(test_set)
+        open(database_index_path + "/test_set.csv", "w").writelines(test_set)
+    if not (train_set_div==1):
+        random.shuffle(train_set)
+        open(database_index_path + "/train_set.csv", "w").writelines(train_set)
+
+
+def docked_ligand_overlaps_with_crystal(database_path,docked_lig_path,tanimoto_cutoff=0.75,clash_cutoff_A=4,clash_size_cutoff=0.3):
+    """sometimes ligand docks correctly into the neighboring pocket of a multimer
+    this script returns true when such an overlap is found"""
+
+    def tanimoto_similarity(cryst_lig_path, docked_lig_path):
+        command = os.popen('babel -d {} {} -ofpt -xfFP4'.format(cryst_lig_path, docked_lig_path))
+        ls = command.read()
+        tanimoto_similarity = re.split('=|\n', ls)[2]
+        return tanimoto_similarity
+
+    def calculate_clash(cryst_lig_path, docked_lig_path,clash_cutoff_A=clash_cutoff_A):
+        """calculates the ratio of atoms between two molecules that appear within van Der Waals cutoff of 4A"""
+        # calculate atom to atom distances
+        prody_docked_ligand = prody.parsePDB(docked_lig_path).getCoords()
+        prody_cryst_ligand = prody.parsePDB(cryst_lig_path).getCoords()
+
+        def atom_clashing(index):
+            # calculate distance to all atoms in the other molecule and check if any of them falls below cutoff
+            if any(np.sum((prody_cryst_ligand[index,:] - prody_docked_ligand)**2,axis=1)**0.5 < clash_cutoff_A):
+                return True
+            else:
+                return False
+
+        clash_mask = map(atom_clashing,np.arange(len(prody_cryst_ligand[:, 0])))
+        return np.average(clash_mask)
+
+    clash_found = False
+    # walk through /crystal/ligands and see if any of them are similar
+    # there should be at least one
+    for dirpath, dirnames, filenames in os.walk(database_path + "/crystal_ligands/" + docked_lig_path.split("/")[-2]):
+        for filename in filenames:
+            if re.search('.pdb$', filename):
+                cryst_lig_path = str(os.path.abspath(dirpath) + "/" + filename)
+                if tanimoto_similarity(cryst_lig_path,docked_lig_path) > tanimoto_cutoff:
+                    # if ligand in crystal is similar to the docked ligand, calculate clash
+                    if (calculate_clash(cryst_lig_path,docked_lig_path) > clash_size_cutoff):
+                        clash_found = True
+    return clash_found
+
 
 
 
 def prepare_labeled_pdb():
     preprocess_PDB_to_npy(database_path='../datasets/labeled_pdb')
     write_database_index_file(database_path='../datasets/labeled_pdb',database_index_path='../datasets/labeled_pdb',lig_dirs=["crystal_ligands","docked_ligands"])
-    split_into_train_and_test_sets(database_index_path='../datasets/labeled_pdb',train_set_div=0.8)
-
+    split_into_train_and_test_sets(database_index_path='../datasets/labeled_pdb',train_set_div=1,replicate_positives=20)
 
 def prepare_labeled_npy():
     write_database_index_file(database_path='../datasets/labeled_npy',database_index_path='../datasets/labeled_npy',lig_dirs=["crystal_ligands","docked_ligands"])
-    split_into_train_and_test_sets(database_index_path='../datasets/labeled_npy',train_set_div=0.8)
-
+    split_into_train_and_test_sets(database_index_path='../datasets/labeled_npy',train_set_div=1,replicate_positives=20)
 
 def prepare_unlabeled_npy():
     write_database_index_file(database_path='../datasets/unlabeled_npy',database_index_path='../datasets/unlabeled_npy',lig_dirs=["ligands"])
     split_into_train_and_test_sets(database_index_path='../datasets/unlabeled_npy',train_set_div=0)
 
+#prepare_labeled_pdb()
 prepare_labeled_npy()
-prepare_unlabeled_npy()
+#prepare_unlabeled_npy()
+
