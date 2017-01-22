49b084aba478430110a619e8ea38f83fa5da6c08
LXander <l.xander.233@gmail.com>
2017-01-22 14:59:45 -0500

    av4 eval pipeline

--- av4_eval.py
+++ av4_eval.py
@@ -1,16 +1,17 @@
-import time,os
+import time, os
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 import re
-from av4_input import image_and_label_queue
-from av4 import FLAGS,max_net
+from av4_eval_input import image_and_label_queue,index_the_database,read_receptor_and_ligand
+from av4 import FLAGS, max_net
 from collections import defaultdict
 
+FLAGS.saved_session = '/home/ubuntu/xiao/code/core/summaries/5_netstate/saved_state-2299'
 
-FLAGS.saved_session = './summaries/1_netstate/saved_state-9000'
+FLAGS.predictions_file_path = re.sub("netstate", "logs", FLAGS.saved_session)
 
-FLAGS.predictions_file_path = re.sub("netstate","logs",FLAGS.saved_session)
+
 
 class store_predictions:
     '''
@@ -19,85 +20,108 @@ class store_predictions:
     '''
 
     raw_predictions = defaultdict(list)
-    reduce_predictions = defaultdict(list)
+    processed_predictions = defaultdict(list)
 
-    def add_batch(self,ligand_file_path,batch_predictions):
-        ligand_file_name = os.path.basename(ligand_file_path).split('.')[0]
-        self.raw_predictions[ligand_file_name].append(batch_predictions)
+    def add_batch(self, in_the_range,ligand_file_paths, frame_ids , batch_predictions):
+        ligand_file_name = map(lambda filename:os.path.basename(filename).split('.')[0],ligand_file_paths)
+
+        for mark,ligand,frame_id,prediction in zip(in_the_range,ligand_file_name,frame_ids,batch_predictions):
+            if mark:
+                self.raw_predictions[ligand].append(prediction)
 
     def reduce(self):
         '''
         if a ligand has more than one predictions
         use mean as final predictions
         '''
-        for key,value in raw_predictions.items():
+        for key, value in self.raw_predictions.items():
 
-            if len(value)>1:
-                predictions_size = map(lambda x:len(x),value)
-                if len(set(predictions_size))>1:
-                    raise Exception(key," has different number of predictions ",set(predictions_size))
-                reduce_predictions[key].append(np.mean(value,axis=0))
+            if len(value) > 1:
+                predictions_size = map(lambda x: len(x), value)
+                if len(set(predictions_size)) > 1:
+                    raise Exception(key, " has different number of predictions ", set(predictions_size))
+                self.processed_predictions[key].append(np.mean(value, axis=0))
             else:
-                reduce_predictions[key].append(value)
+                self.processed_predictions[key].append(value)
 
-    def final_predictions(self,predictions_list):
-        length = min(len(predictions_list,10))
+    def final_predictions(self, predictions_list):
+        length = min(len(predictions_list, 10))
         return np.mean(predictions_list[:length])
 
+    def fill_na(self):
+        for key in self.raw_predictions.keys():
+            value_len = len(self.raw_predictions[key])
+            if value_len>FLAGS.top_k:
+                print "{} have more predictions than expected, {} reuqired {} found.".format(key,FLAGS.top_k,value_len)
+            else:
+                for i in range(FLAGS.top_k-value_len):
+                    self.raw_predictions[key]
+
+
     def save_predictions(self):
-        records =[]
-        for key,value in self.reduce_predictions.items():
-            records.append(key,self.final_predictions(value))
+        records = []
+        for key, value in self.raw_predictions.items():
+            records.append([key]+ value)
 
-        submission_csv = pd.DataFrame(records,columns=['Id','Predicted'])
-        submission_csv.to_csv(FLAGS.predictions_file_path+'_submission.csv',index=False)
-
+        submission_csv = pd.DataFrame(records, columns=['Id']+[ 'Predicted_%d'%i for i in range(1,len(records[0]))])
+        submission_csv.to_csv(FLAGS.predictions_file_path + '_submission.csv', index=False)
 
     def save(self):
-        self.reduce()
+        self.fill_na()
         self.save_predictions()
 
-
 def evaluate_on_train_set():
-    "train a network"
-
-    # create session which all the evaluation happens in
     sess = tf.Session()
 
-    ligand_file_name,(_,y_,x_image_batch) = image_and_label_queue(sess = sess,batch_size=FLAGS.batch_size,
-                                               pixel_size=FLAGS.pixel_size,side_pixels=FLAGS.side_pixels,
-                                               num_threads=FLAGS.num_threads,database_path=FLAGS.test_set_path)
+    current_epoch,filename_batch,in_the_ranges ,label_batch, image_batch = image_and_label_queue(sess=sess, batch_size=FLAGS.batch_size,
+                                                                    pixel_size=FLAGS.pixel_size,
+                                                                    side_pixels=FLAGS.side_pixels,
+                                                                    num_threads=FLAGS.num_threads,
+                                                                    database_path=FLAGS.test_set_path,
+                                                                    num_epochs=FLAGS.num_epochs)
 
-    float_image_batch = tf.cast(x_image_batch,tf.float32)
-    batch_size = tf.shape(x_image_batch)
+    float_image_batch = tf.cast(image_batch,tf.float32)
 
     keep_prob = tf.placeholder(tf.float32)
-    y_conv = max_net(float_image_batch,keep_prob)
+    predicted_labels = max_net(float_image_batch,keep_prob)
 
-    # compute softmax over raw predictions
-    predictions = tf.nn.softmax(y_conv)[:,1]
-    # restore variables from sleep
+    positive_labels = tf.nn.softmax(predicted_labels)[:,1]
+
+    batch_size = tf.shape(label_batch)
+
     saver = tf.train.Saver()
     saver.restore(sess,FLAGS.saved_session)
-    
+
+    sess.run(tf.global_variables_initializer())
+    #sess.run(tf.initialize_local_variables())
 
     coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(sess = sess,coord=coord)
-
-
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
     # create a variable to store all predicitons
-    all_predictios = store_predictions()
+    all_predictions = store_predictions()
     batch_num = 0
-    print "start eval..." 
-    while True or not coord.should_stop():
-        
-        batch_shape,test_ligand,test_predictions = sess.run([batch_size,ligand_file_name,predictions],feed_dict={keep_prob:1})
-        all_predictios.add_batch(test_ligand,test_predictions)
-        batch_num +=1
-        print "batch num",batch_num,
-        print "\tbatch size",batch_shape[0]
-    all_predictios.save()
+    print "start eval..."
+
+    while not coord.should_stop():
+        batch_shape, mark,test_ligands, test_frame_ids, test_predictions = sess.run(
+            [batch_size, in_the_ranges,filename_batch, current_epoch, positive_labels], feed_dict={keep_prob: 1})
+        all_predictions.add_batch(mark,test_ligands, test_frame_ids, test_predictions)
+        batch_num += 1
+        print "batch num", batch_num,
+        print "\tbatch size", batch_shape[0]
+        print test_ligands[0], test_frame_ids[0], test_predictions[0]
+        print test_frame_ids
+        print mark
+        print "min epoch",min(test_frame_ids)
+        if min(test_frame_ids)>FLAGS.top_k:
+            break;
+
+    print "+++++++++++++++++++++++++++++++++++over++++++++++++++++++++++++"
+    coord.request_stop()
+    coord.join(threads, stop_grace_period_secs=5)
+
+    all_predictions.save()
 
 
 evaluate_on_train_set()
