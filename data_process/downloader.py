9ad09f3968efe7391d086645b36145001c2175bb
LXander <l.xander.233@gmail.com>
2017-02-11 11:14:02 -0500

    modify downloader

--- data_process/downloader.py
+++ data_process/downloader.py
@@ -10,38 +10,70 @@ import threading
 
 
 class parseRCSB:
+    '''
+    Download pdb from rcsb and split it into receptor and ligand
+    '''
     def __init__(self):
-       pass
+        self.get_address = lambda PDB: 'https://files.rcsb.org/download/' + PDB + '.pdb'
+        self.thread_num = FLAGS.thread_num
+        self.process_num = FLAGS.process_num
+        self.log_file = FLAGS.log_file
+        self.get_dataframe()
 
-
+    def get_dataframe(self):
+        content = open('target_PDB.txt').readline()
+        content = content.split(',')
+        content = map(lambda x: x.strip(), content)
+        self.pdb_list = content
 
     def error_log(self, content):
-        with open(FLAGS.log_file, 'a') as fout:
+        # write down error information
+        with open(self.log_file, 'a') as fout:
             fout.write(content)
 
     def downloads(self, item):
+        '''
+        Download pdb from rcsb and split it into receptor and ligand
+        :param item: 4 letter PDB ID '3EML'
+        :return:
+        '''
 
-        os.system('cd {}'.format(FLAGS.rowdata_folder))
-        address = FLAGS.address(item)
-        os.system('wget {}'.format(address))
+        # Download pdb to rowdata_folder
+        download_address = self.get_address(item)
+        os.system('wget -P {} {}'.format(FLAGS.rowdata_folder,download_address))
 
+        # create folder to store ligand
         pdbname = item.lower()
         ligand_folder = os.path.join(FLAGS.splited_ligand_folder, pdbname)
         if not os.path.exists(ligand_folder):
             os.mkdir(ligand_folder)
 
+        # parse pdb
         try:
             parsed = prody.parsePDB(os.path.join(FLAGS.rowdata_folder, item + '.pdb'))
         except:
             self.error_log('can not parse {}.\n'.format(item))
             return None
 
+        # select receptor and ligand
         hetero = parsed.select('(hetero and not water) or resname ATP or resname ADP')
         receptor = parsed.select('protein or nucleic')
 
+
+        if receptor is None:
+            self.error_log("{} doesn't have receptor.\n".format(item))
+            return None
+
+        if hetero is None:
+            self.error_log("{} doesn't have ligand.\n".format(item))
+            return None
+
+
         ligand_flags = False
+
         for each in prody.HierView(hetero).iterResidues():
-            if each.numAtoms() <= 10:
+            if each.numAtoms() <= FLAGS.atom_num_threahold:
+                # ignore ligand if atom num is less than threshold
                 continue
             else:
                 ligand_flags = True
@@ -56,70 +88,53 @@ class parseRCSB:
             receptor_path = os.path.join(FLAGS.splited_receptor_folder, pdbname + '.pdb')
             prody.writePDB(receptor_path, receptor)
         else:
-            self.error_log("{} doesn't convert, not ligand have more than 10 atoms.\n")
-
+            self.error_log("{} doesn't convert, no ligand have more than 10 atoms.\n")
 
     def thread_convert(self, func, dataframe, index):
-
         for i in index:
             func(dataframe[i])
 
     def process_convert(self, func, dataframe, index):
-
         # linspace contain end value but range don't
         # so we use edge[i+1] to select value in index
         # end should be len(index)-1
 
-
-        if len(index) < FLAGS.thread_num:
+        if len(index) < self.thread_num:
             for i in index:
                 func(dataframe[i])
             return
 
-        edge = np.linspace(0, len(index) - 1, FLAGS.thread_num + 1).astype(int)
-
+        edge = np.linspace(0, len(index) - 1, self.thread_num + 1).astype(int)
         thread_list = [threading.Thread(target=self.thread_convert,
                                         args=(func,
                                               dataframe,
                                               range(index[edge[i]],
                                                     index[edge[i + 1]])))
-                       for i in range(FLAGS.thread_num)]
+                       for i in range(self.thread_num)]
 
         for t in thread_list:
-            # print "thread start: ",t
             t.start()
 
         for t in thread_list:
             t.join()
 
     def convert(self):
-        '''
-        according the result of 'database_from_csv'
-        running multiprocess to get result
-
-        :param dataframe: pandas.DataFrame
-                          string
-        :param coded: bool
-        :return:
-        '''
 
         convert_func = self.downloads
 
-
-
         # when there's not enough entry to comvert , decrease thread's num
-        if len(FLAGS.pdb_list) < FLAGS.process_num * FLAGS.thread_num:
-
-            for i in range(len(FLAGS.pdb_list)):
-                convert_func(FLAGS.pdb_list[i])
+        if len(self.pdb_list) < self.process_num * self.thread_num:
+            for i in range(len(self.pdb_list)):
+                convert_func(self.pdb_list[i])
             return
-        edge = np.linspace(0, len(FLAGS.pdb_list), FLAGS.process_num + 1).astype(int)
+
+        edge = np.linspace(0, len(self.pdb_list), self.process_num + 1).astype(int)
         process_list = [multiprocessing.Process(target=self.process_convert,
                                                 args=(convert_func,
-                                                      FLAGS.pdb_list,
+                                                      self.pdb_list,
                                                       range(edge[i],
                                                             edge[i + 1])))
-                        for i in range(FLAGS.process_num)]
+                        for i in range(self.process_num)]
 
         for p in process_list:
             print "process start: ", p
@@ -135,22 +150,14 @@ class FLAGS:
     rowdata_folder = os.path.join(workplace, 'row')
     splited_receptor_folder = os.path.join(workplace, 'row_receptor')
     splited_ligand_folder = os.path.join(workplace, 'ligands')
-    address = lambda PDB: 'https://files.rcsb.org/download/' + PDB + '.pdb'
+
     log_file = 'error.log'
     thread_num = 16
     process_num = 12
+    atom_num_threahold = 10
 
-
-def parse_FLAG():
-
-
-    content = open('target_PDB.txt').readline()
-    content = content.split(',')
-    content = map(lambda x: x.strip(), content)
-    FLAGS.pdb_list = content
 
 
 if __name__ == '__main__':
-    parse_FLAG()
     parser = parseRCSB()
     parser.convert()
