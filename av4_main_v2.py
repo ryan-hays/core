388ad5fa0dec0db9fb24890253b01f1a0adcbcc9
LXander <l.xander.233@gmail.com>
2017-02-21 04:56:41 -0500

    test v2 output

--- av4_main_v2.py
+++ av4_main_v2.py
@@ -7,6 +7,29 @@ from av4_networks_v2 import *
 
 # telling tensorflow how we want to randomly initialize weights
 
+def output_test():
+    sess = tf.Session()
+    filename_queue, examples_in_database = index_the_database_into_queue(FLAGS.database_path, shuffle=True)
+    # create an epoch counter
+    batch_counter = tf.Variable(0)
+    batch_counter_increment = tf.assign(batch_counter, tf.Variable(0).count_up_to(
+        np.round((examples_in_database * FLAGS.num_epochs) / FLAGS.batch_size)))
+    epoch_counter = tf.div(batch_counter * FLAGS.batch_size, examples_in_database)
+    # epoch_counter = tf.constant(0)
+    # create a custom shuffle queue
+    _, current_epoch, label, sparse_image_batch = image_and_label_queue(batch_size=FLAGS.batch_size,
+                                                                        pixel_size=FLAGS.pixel_size,
+                                                                        side_pixels=FLAGS.side_pixels,
+                                                                        num_threads=FLAGS.num_threads,
+                                                                        filename_queue=filename_queue,
+                                                                        epoch_counter=epoch_counter)
+
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+
+    image,image_label = sess.run([sparse_image_batch,label])
+    print image_label
+
 def train():
     "train a network"
     # it's better if all of the computations use a single session
@@ -134,8 +157,8 @@ def main(_):
         tf.gfile.MakeDirs(summaries_dir + "/" + str(FLAGS.run_index) + '_test')
         tf.gfile.MakeDirs(summaries_dir + "/" + str(FLAGS.run_index) + '_netstate')
         tf.gfile.MakeDirs(summaries_dir + "/" + str(FLAGS.run_index) + '_logs')
-    train()
-
+    #train()
+    output_test()
 
 if __name__ == '__main__':
     tf.app.run()
