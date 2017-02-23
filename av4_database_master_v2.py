2a6d166aa5d3ac085736477b1ad3c5164d383a2a
LXander <l.xander.233@gmail.com>
2017-02-22 23:51:53 -0500

    db master for dude

--- av4_database_master_v2.py
+++ av4_database_master_v2.py
@@ -16,47 +16,80 @@ class stats:
 # writes statistics
 # TODO make exceptions more informative
 
-def convert_dude_database_to_av4(database_path,positives_folder,decoys_folder,receptors_folder):
+def convert_dude_database_to_av4(database_path,positives_folder=None,decoys_folder=None,receptors_folder=None):
     output_path = str(database_path+'_av4')
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
     def save_av4(filepath, labels, elements, multiframe_coords):
-    labels = np.asarray(labels, dtype=np.int32)
-    elements = np.asarray(elements, dtype=np.int32)
-    multiframe_coords = np.asarray(multiframe_coords, dtype=np.float32)
+        labels = np.asarray(labels, dtype=np.int32)
+        elements = np.asarray(elements, dtype=np.int32)
+        multiframe_coords = np.asarray(multiframe_coords, dtype=np.float32)
 
-    if not (int(len(multiframe_coords[:, 0]) == int(len(elements)))):
-        raise Exception('Number of atom elements is not equal to the number of coordinates')
+        if not (int(len(multiframe_coords[:, 0]) == int(len(elements)))):
+            raise Exception('Number of atom elements is not equal to the number of coordinates')
 
-    if multiframe_coords.ndim == 2:
-        if not int(len(labels)) == 1:
-            raise Exception('Number labels is not equal to the number of coordinate frames')
-    else:
-        if not (int(len(multiframe_coords[0, 0, :]) == int(len(labels)))):
-            raise Exception('Number labels is not equal to the number of coordinate frames')
+        if multiframe_coords.ndim == 2:
+            if not int(len(labels)) == 1:
+                raise Exception('Number labels is not equal to the number of coordinate frames')
+        else:
+            if not (int(len(multiframe_coords[0, 0, :]) == int(len(labels)))):
+                raise Exception('Number labels is not equal to the number of coordinate frames')
 
-    number_of_examples = np.array([len(labels)], dtype=np.int32)
-    av4_record = number_of_examples.tobytes()
-    av4_record += labels.tobytes()
-    av4_record += elements.tobytes()
-    av4_record += multiframe_coords.tobytes()
-    f = open(filepath + ".av4", 'w')
-    f.write(av4_record)
-    f.close()
+        number_of_examples = np.array([len(labels)], dtype=np.int32)
+        av4_record = number_of_examples.tobytes()
+        av4_record += labels.tobytes()
+        av4_record += elements.tobytes()
+        av4_record += multiframe_coords.tobytes()
+        f = open(filepath + ".av4", 'w')
+        f.write(av4_record)
+        f.close()
 
     for receptor in os.listdir(database_path):
         receptor_folder = os.path.join(database_path,receptor)
         for ligand in os.listdir(receptor_folder):
             try:
-                ligand_folder = os.path.join(os.path.join(receptor_folder,ligand)
+                ligand_folder = os.path.join(os.path.join(receptor_folder,ligand))
                 receptor_path = os.path.join(os.path.dirname(database_path),'receptors',receptor+'.pdb')
                 frames = os.listdir(ligand_folder)
                 frame_num = len(frames)
                 prody_receptor = prody.parsePDB(receptor_path)
-                multiframe_ligand_coords = np.array([])
-                prody_positive = prody.parsePDB(os.path.join(ligand_folder,ligand+'_1.pdb'))
-            
+
+                for i in range(1,frame_num+1):
+                    prody_positive = prody.parsePDB(os.path.join(ligand_folder, ligand + '_' + i + '.pdb'))
+                    if i == 1:
+                        multiframe_ligand_coords = prody_positive.getCoords()
+                        labels = np.array([1])
+                        ligand_elements = np.asarray(prody_positive.getElements())
+                    else:
+                        multiframe_ligand_coords = np.dstack(
+                                    (multiframe_ligand_coords, prody_positive.getCoords()))
+                        labels = np.concatenate((labels,[0]))
+
+                        if not all(np.assarray(prody_positive.getElements())==ligand_elements):
+                            raise Exception('attempting to add ligand with different order of atoms')
+            except Exception as e:
+                print e
+                stats.ligands_failed +=1
+                print "ligands parsed:", stats.ligands_parsed, "ligands failed:", stats.ligands_failed
+                continue
+
+            stats.ligands_parsed+=1
+            print "ligands parsed:", stats.ligands_parsed, "ligands failed:", stats.ligands_failed
+
+            dest_folder = os.path.join(output_path,receptor)
+            if not os.path.exists(dest_folder):
+                os.mkdir(dest_folder)
+
+            def atom_to_number(atomname):
+                atomic_tag_number = atom_dictionary.ATM[atomname.lower()]
+                return atomic_tag_number
+
+            receptor_elements = map(atom_to_number, prody_receptor.getElements())
+            ligand_elements = map(atom_to_number, ligand_elements)
+
+            save_av4(os.path.join(dest_folder,receptor), [0], receptor_elements, prody_receptor.getCoords())
+            save_av4(os.path.join(dest_folder,ligand), labels, ligand_elements, multiframe_ligand_coords)
 
 
 def convert_database_to_av4(database_path, positives_folder, decoys_folder, receptors_folder):
@@ -122,6 +155,6 @@ def convert_database_to_av4(database_path, positives_folder, decoys_folder, rece
 
 
 
-convert_database_to_av4(database_path="/home/ubuntu/xiao/data/newkaggle/dude/crystal/crystal_ligands", positives_folder="crystal_ligands",
-                        decoys_folder="docked_ligands", receptors_folder='receptors')
-
+#convert_database_to_av4(database_path="/home/ubuntu/xiao/data/newkaggle/dude/crystal/crystal_ligands", positives_folder="crystal_ligands",
+#                        decoys_folder="docked_ligands", receptors_folder='receptors')
+convert_dude_database_to_av4(database_path="/home/ubuntu/xiao/data/newkaggle/dude/")
