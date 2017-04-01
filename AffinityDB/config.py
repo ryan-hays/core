4029bbcab18756343d8f4aa6d27ef75f40e7efa7
Maksym Korablyov <mkorablyov@gmail.com>
2017-04-01 17:36:54 -0400

    revision of XIao's code 2

--- AffinityDB/config.py
+++ AffinityDB/config.py
@@ -9,7 +9,7 @@ Parameter
 # only ligands' atom number above threshold will saved
 heavy_atom_threshold = 0
 # number of process running at the same time
-process_num = 4
+process_num = 16
 
 # RENAMES:
 # [ ] tanimoto_cutoff    # minimum Tanimoto similarity score to be considered 
@@ -31,7 +31,39 @@ process_num = 4
 # [x] think how we can deal with multiple docking parameters
 
 
+# _________________________ Review 2 _________________________________________________
 
+
+# Bugs
+# 0) Tanimoto similarity cutoff does not work
+# 1) Fraction of native contacts does not work
+# 2) similarity cutoff, fraction of native contacts, clash -- all should have a similar simple format
+# 3) reorder should become a part of every docking algorithm
+# 4) get_same_ligands becomes standalone
+# 5) ligand_name_of becomes _ligand_name_of same for receptor_of, receptor_path_of, mkdir, run ---> run_multiptocessor
+# 6) calculate_clash becomes a part of detect_overlap
+# 7) dock merges with VInardo_do_docking
+# 8) Empty files + broken file check (have a log file to do the stop so as to be able to resume from any point )
+# broken file is unless "success is written in log"
+
+# Enhancements
+# 0) Dependencies into README.md
+# 1) rename into 1_download, 2_split, 3_docking
+# 2) split_structure needs medhod (IE: NMR,X-RAY)
+
+# 3) More splitting statistics
+# let's also write a number of receptor atoms when splitting
+# and the number of receptor chains when splitting
+# number of receptor residues when splitting
+
+# 4) write a script that will gether all of the logs together and create many plots about the data in python notebook
+# Statistics about the PDB in README.md with all of the plots
+# def crystal_ligand_for_same_receptor
+
+
+# 5) Write the script that will do a retrieval based on 4 and write the structures in av4 format
+
+# 6) How do we do this on Orchestra or Bridges -- how do we launch many separate jobs ?
 
 """
 overlap detaction constant
@@ -45,7 +77,7 @@ clash_size_cutoff = 0.3 # __ an exact value should be recorded
 Folders
 """
 #base folder for all the output
-database_root = '/home/xander/affinityDB/test'
+database_root = '/home/maksym/PyCharmProjects/tensorMe21_Database/AffinityDB/test'
 
 # pdb download from Protein DataBank
 pdb_download_path = os.path.join(database_root,'data','row_pdb')
@@ -72,12 +104,12 @@ File Path
 """
 
 # path of smina binary file
-smina = '/home/xander/Program/smina/smina.static'
+smina = '/usr/local/bin/smina'
 
 
 # pdb_target_list
 #target_list_file = os.path.join(sys.path[0],'target_list','main_pdb_target_list.txt')
-target_list_file = '/home/xander/affinityDB/target_list/main_pdb_target_list.txt'
+target_list_file = '/home/maksym/PyCharmProjects/tensorMe21_Database/AffinityDB/target_list/maksym_test.txt'
 
 # example scoring
 scoring_terms = os.path.join(sys.path[0], 'scoring', 'smina.score')
