54ebe4a7b4853dce0c765bd94939dafb92788c1d
Maksym Korablyov <mkorablyov@gmail.com>
2017-04-10 14:56:47 -0400

    comments to separate TF from python

--- deepVS_net_41017.py
+++ deepVS_net_41017.py
@@ -4,6 +4,8 @@ from deepVS_input import *
 
 #---------------------------------HYPERPARAMETERS---------------------------------#
 
+# todo: (maksym) hyperparameters should be a class (like FLAGS)
+
 #atom type embedding size
 d_atm = 200
 #amino acid embedding size
@@ -39,27 +41,17 @@ def bias_variable(shape):
     initial = tf.constant(0.01, shape=shape)
     return tf.Variable(initial)
 
-def variable_summaries(var, name):
-    """attaches a lot of summaries to a tensor."""
-    with tf.name_scope('summaries'):
-        mean = tf.reduce_mean(var)
-        tf.summary.scalar('mean/' + name, mean)
-    with tf.name_scope('stddev'):
-        stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-        tf.summary.scalar('stddev/' + name, stddev)
-        tf.summary.scalar('max/' + name, tf.reduce_max(var))
-        tf.summary.scalar('min/' + name, tf.reduce_min(var))
-        tf.summary.histogram(name, var)
+
 
 def embed_layer(layer_name, input, num_atoms):
 	"""transforms the mapped z into feature vectors and returns the resulting tensor"""
 	#input is standard matrix of shape m * k_c * 2
-	#output is tensor of shape k_c * d_atm+d_dist * m
-	with tf.name_scope(layer_name):
-		with tf.name_scope('atom_weights'):
-			W_atom = weight_variable([ATOM_TYPES, d_atm])
-		with tf.name_scope('dist_weights'):
-			W_dist = weight_variable([DIST_BINS, d_dist])
+
+#TODO: (maksym) move to TF and out of this pythonic function
+#	W_atom = weight_variable([ATOM_TYPES, d_atm])
+#	W_dist = weight_variable([DIST_BINS, d_dist])
+
+	#------------------------------- python ---------------------------------------------------------------
 	for atom_index in range(len(input)):
 		for neighbor_index in range(len(input[0])):
 			atom_type = input[atom_index][0][0]
@@ -75,47 +67,12 @@ def embed_layer(layer_name, input, num_atoms):
 			embedded_input = tf.reshape(face, [k_c, d_atm+d_dist, 1])
 		else:
 			#turns embedded input into k_c * 400 * m tensor
-			embedded_input = tf.concat([embedded_input, tf.reshape(face, [k_c, d_atm+d_dist, 1])], 2)
-	#gives embedded_input the depth dimension and batch size
-	return tf.reshape(embedded_input, [1, k_c, d_atm+d_dist, num_atoms, 1])
+# TODO: use np.append instead of TensorFLow
+#			embedded_input = tf.concat([embedded_input, tf.reshape(face, [k_c, d_atm+d_dist, 1])], 2)
+#	#gives embedded_input the depth dimension and batch size
+#	return tf.reshape(embedded_input, [1, k_c, d_atm+d_dist, num_atoms, 1])
 
-def conv_layer(layer_name, input_tensor, filter_size, strides=[1,1,1,1,1], padding='SAME'):
-	"""makes a simple face convolutional layer"""
-	with tf.name_scope(layer_name):
-		with tf.name_scope('weights'):
-			W_conv = weight_variable(filter_size)
-			variable_summaries(W_conv, layer_name + '/weights')
-		with tf.name_scope('biases'):
-			b_conv = bias_variable([filter_size[3]])
-			variable_summaries(b_conv, layer_name + '/biases')
-		h_conv = tf.nn.conv3d(input_tensor, W_conv, strides=strides, padding=padding) + b_conv
-		tf.summary.histogram(layer_name + '/pooling_output', h_conv)
-	print layer_name,"output dimensions:", h_conv.get_shape()
-	return h_conv
 
-def pool_layer(layer_name, input_tensor, num_atoms):
-	"""makes a max pool layer that returns the max of each column"""
-	with tf.name_scope(layer_name):
-		h_pool = tf.nn.max_pool3d(input_tensor, ksize=[1,1,1,num_atoms,1], strides=[1,1,1,1,1], padding='VALID')
-		tf.summary.histogram(layer_name + '/max_pooling_output', h_pool)
-	print layer_name, 'output dimensions:', h_pool.get_shape()
-	return h_pool
-
-def fc_layer(layer_name,input_tensor,output_dim):
-	"""makes a simple fully connected layer"""
-	input_dim = int((input_tensor.get_shape())[1])
-
-	with tf.name_scope(layer_name):
-		weights = weight_variable([input_dim, output_dim])
-		variable_summaries(weights, layer_name + '/weights')
-	with tf.name_scope('biases'):
-		biases = bias_variable([output_dim])
-		variable_summaries(biases, layer_name + '/biases')
-	with tf.name_scope('Wx_plus_b'):
-		h_fc = tf.matmul(input_tensor, weights) + biases
-		tf.summary.histogram(layer_name + '/fc_output', h_fc)
-	print layer_name, "output dimensions:", h_fc.get_shape()
-	return h_fc
 
 #-----------------------------------NETWORK----------------------------------------#
 
@@ -126,6 +83,8 @@ def fc_layer(layer_name,input_tensor,output_dim):
 
 class Z(object):
 
+# todo: remove this since this operation has been performed
+"""
 	def atom_dictionary(self):
 		ATM = {}
 
@@ -152,7 +111,7 @@ class Z(object):
 		return ATM
 
 	def __init__(self, ligand_atoms, ligand_coords):
-            """
+
             :param ligand_atoms: a set of all atoms in our ligand. in the form of characters
             :param ligand_coords: a dictionary mapping atoms to its coordinate as a tuple. for example "c": (0,0,0)
             :param kc: an int - the number of neighbors we want to consider
@@ -163,12 +122,12 @@ class Z(object):
                 each "face" of the matrix is the kc closest neighbors for an atom A. We have m of these faces - one for each atom
                 Each row in a face is [neighbor, distance] - the neighbor to A, and its distance to A
                     we have kc rows in each face.
-            """
+
 		self.ligand_atoms = ligand_atoms
 		self.ligand_coords = ligand_coords
 		self.atom_map = self.atom_dictionary()
 		self.z = self.build_z()
-
+"""
 	def convert_coords_to_distances(self, start_atom):
 		ligand_distances = {}
 		atom_coord = self.ligand_coords[start_atom]
@@ -212,23 +171,23 @@ class Z(object):
 		return raw_z
 
 
-def deepVS_net(ligand_atoms, ligand_coords, keep_prob):
+#def deepVS_net(ligand_atoms, ligand_coords, keep_prob):
 	#gets a standard array of dimensions m * k_c * 2
-	input = Z(ligand_atoms, ligand_coords).z
-	#do the feature embedding to get a k_c * (d_atm + d_dist) * m TENSOR
-	z = embed_layer('embed_layer', input, len(input))
-	#convolutional layer - padding = 'VALID' prevents 0 padding
-	z_conv = conv_layer('face_conv', input_tensor=z, filter_size=[k_c, d_atm+d_dist, 1, 1, cf], padding='VALID')
-	#max pool along the columns (corresponding to each convolutional filter)
-	z_pool = pool_layer(layer_name='pool_column', input_tensor=z_conv, num_atoms=len(input))
-	#pool gives us batch*1*1*1*cf tensor; flatten it to get a tensor of length cf
-	z_flattened = tf.reshape(z_pool, [-1, cf])
-	#fully connected layer
-	z_fc1 = fc_layer(layer_name='fc1', input_tensor=z_flattened, output_dim=h)
-	#dropout
-	#output layer
-	z_output = fc_layer(layer_name='out_neuron', input_tensor=z_fc1, output_dim=2)
-	return z_output
+	# input = Z(ligand_atoms, ligand_coords).z
+	# #do the feature embedding to get a k_c * (d_atm + d_dist) * m TENSOR
+	# z = embed_layer('embed_layer', input, len(input))
+	# #convolutional layer - padding = 'VALID' prevents 0 padding
+	# z_conv = conv_layer('face_conv', input_tensor=z, filter_size=[k_c, d_atm+d_dist, 1, 1, cf], padding='VALID')
+	# #max pool along the columns (corresponding to each convolutional filter)
+	# z_pool = pool_layer(layer_name='pool_column', input_tensor=z_conv, num_atoms=len(input))
+	# #pool gives us batch*1*1*1*cf tensor; flatten it to get a tensor of length cf
+	# z_flattened = tf.reshape(z_pool, [-1, cf])
+	# #fully connected layer
+	# z_fc1 = fc_layer(layer_name='fc1', input_tensor=z_flattened, output_dim=h)
+	# #dropout
+	# #output layer
+	# z_output = fc_layer(layer_name='out_neuron', input_tensor=z_fc1, output_dim=2)
+	# return z_output
 
 
 
